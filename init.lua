52201eaade52cf690be19e5f984581851bfd498d
Omar Ali <61897650+omar0ali@users.noreply.github.com>
2024-08-31 13:23:41 -0400

    Removed none-ls, caused problems

--- init.lua
+++ init.lua
@@ -3,14 +3,14 @@ vim.g.mapleader = " " -- <leader> space bar
 -- Setup Lazy
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins") -- Load from plugins folder (Or provide a folder that has files) All of the files starts with return {...}
